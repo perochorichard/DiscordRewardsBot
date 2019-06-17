aceae46bcb1a17b82b801f7b6d1a53aa6d67f3fd
perochorichard <IAmTheDeveloperNow@gmail.com>
2019-06-17 18:26:05 -0400

    fixed command not found bug. added key generation

--- src/model/Utility.js
+++ src/model/Utility.js
@@ -3,14 +3,24 @@ exports.processCommand = (recievedMessage) => {
     const splitCommand = fullCommand.split(" "); // split message into a collection
     const primaryCommand = splitCommand[0]; // word immediately after command prefix
     const arguments = splitCommand.slice(1); // all parameters after primary command
-
-    try {
-        require("./commands/" + primaryCommand).run(recievedMessage); // TODO: change to global path instead of relative path
-    } catch (e) {
+    
+    let command = getCommand(primaryCommand);
+    if (command === null) {
         recievedMessage.reply("cannot find command: " + primaryCommand);
+        return;
     }
+    command.run(recievedMessage);
 }
 
 exports.getArguments = (recievedMessage) => {
     return recievedMessage.content.substr(1).split(" ").slice(1);
+}
+
+getCommand = (primaryCommand) => {
+    try {
+        let command = require("./commands/" + primaryCommand);
+        return command;
+    } catch {
+        return null;
+    }
 }
--- src/model/commands/create.js
+++ src/model/commands/create.js
@@ -10,24 +10,50 @@ exports.run = (recievedMessage) => {
     const amount = Number(args[0]);
     const points = Number(args[1]);
 
-    generateKey(points);
+    let keyObj = generateKeyObj(points);
+    recievedMessage.reply("your new key is: " + keyObj.key + "\n" +
+    "key points: " + keyObj.targetPoints);
 }
 
-generateKey = (points) => {
-    // READING
-    const fs = require("fs");
-    fs.readFile("src/hits.txt", "utf8", function (err, buf) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(buf);
-    });
+generateKeyObj = (points) => {
+    let keyObj = { targetPoints: points, key: randomKey() };
+    return keyObj;
+}
 
-    // WRITING
-    var data = "New File Contents";
-    fs.writeFile("temp.txt", data, (err) => {
-        if (err) console.log(err);
-        console.log("Successfully Written to File.");
-    });
+randomKey = () => {
+    let key = '';
+    for (let i = 0; i < 4; i++) {
+        key += randomSequence() + "-";
+    }
+    key = key.substr(0, key.length - 1);
+    return key;
+}
+
+randomSequence = () => {
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let sequenceLength = 5;
+    let sequence = '';
+    for (let i = 0; i < sequenceLength; i++) {
+        sequence += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return sequence;
+}
+
+FileIO = () => {
+        // READING
+        const fs = require("fs");
+        fs.readFile("src/hits.txt", "utf8", function (err, buf) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(buf);
+        });
+    
+        // WRITING
+        let data = "hello world";
+        fs.writeFile("temp.txt", data, (err) => {
+            if (err) console.log(err);
+            console.log("Successfully Written to File.");
+        });
 }
